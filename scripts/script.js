88749f85453665545cf8cf6a26f2c9387e0e08fd
Jennifer Ye <yej52@mcmaster.ca>
2023-06-27 22:36:20 -0400

    adds mobile drawer animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,34 @@
 const img = document.getElementById("me");
 const about_project = document.getElementById("about_project");
 
-about_project.onclick = function () {
-  let src = img.getAttribute("src");
-  if (src === "../assets/about img.jpg") {
-    img.setAttribute("src", "../assets/craft.jpg");
-  } else {
-    img.setAttribute("src", "../assets/about img.jpg");
-  }
-};
-
-function openNav() {
-  document.getElementById("sideNav").style.width = "15rem";
+if(about_project != null){
+  about_project.onclick = function () {
+    let src = img.getAttribute("src");
+    if (src === "../assets/about img.jpg") {
+      img.setAttribute("src", "../assets/craft.jpg");
+    } else {
+      img.setAttribute("src", "../assets/about img.jpg");
+    }
+  };
 }
 
-function closeNav() {
-  document.getElementById("sideNav").style.width = "0";
+const navBtn = document.querySelector('.menu');
+const nav = document.querySelector('.nav-m');
+const close = document.querySelector('.closebtn');
+
+if(navBtn != null){
+  navBtn.addEventListener('click', function(){
+    nav.classList.remove("hide-active");
+    nav.style.visibility = "visible";
+    nav.classList.toggle("overlay-active");
+  })
+}
+
+if(close != null){
+  close.addEventListener('click', function(){
+    nav.classList.add("hide-active");
+    nav.classList.remove("overlay-active");
+   
+
+  })
 }
