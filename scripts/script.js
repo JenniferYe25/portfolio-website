274f470ad08ef816edf1d5d2c61678cb2df62972
Jennifer Ye <yej52@mcmaster.ca>
2023-06-28 21:32:43 -0400

    temp code for typing effect

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,11 @@ if(about_project != null){
   };
 }
 
+/*
+The following code was adapted from 
+https://www.javascriptfreecode.com/Side_Navigation_with_Overlay_Effect.htm
+Modifications were made to fit the use of the website
+*/
 const navBtn = document.querySelector('.menu');
 const nav = document.querySelector('.nav-m');
 const close = document.querySelector('.closebtn');
@@ -31,4 +36,33 @@ if(close != null){
    
 
   })
+}
+
+const index1 = document.querySelector('.one p');
+const index2 = document.querySelector('.two p');
+const submit = document.querySelector('.button');
+const grid = document.querySelector('.grid');
+
+if(index1 != null && index2 != null){
+  words = [index1.textContent, index2.textContent];
+  elements = [index1, index2]
+}
+
+
+function printChar(word, element) {
+  console.log(word);
+  let i = 0;
+  element.textContent = "";
+  let id = setInterval(() => {
+  if (i >= word.length) {
+  clearInterval(id);
+  } else {
+  element.innerHTML += word[i];
+  i++;
+  }
+  }, 100);
+}
+
+for (let i = 0; i < words.length; i++) {
+  printChar(words[i], elements[i])
 }
