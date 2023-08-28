2545b81d6954639c6493a14eb6466fc1823ad753
Jennifer Ye <yej52@mcmaster.ca>
2023-08-28 10:09:00 -0400

    adds new photo and new info to about me page

--- scripts/about-me.js
+++ scripts/about-me.js
@@ -4,7 +4,7 @@ const about_project = document.getElementById("about_project");
 if (about_project) { // if element exists
   about_project.addEventListener("click", function () { //if clicked change image
     const src = img.getAttribute("src");
-    const newSrc = src === "../assets/about img.jpg" ? "../assets/craft.jpg" : "../assets/about img.jpg";
+    const newSrc = src === "../assets/me.jpg" ? "../assets/craft.jpg" : "../assets/me.jpg";
     img.setAttribute("src", newSrc);
   });
 }
